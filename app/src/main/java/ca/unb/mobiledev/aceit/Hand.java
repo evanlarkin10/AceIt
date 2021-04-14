e5cf8276b1af7e3de5f4cf8ec7dfba7645af862a
nickbarbour <nicholas.barbour12>
2021-04-14 14:37:52 -0300

    Rid fix and added hand class

---
+++ app/src/main/java/ca/unb/mobiledev/aceit/Hand.java
@@ -0,0 +1,48 @@
+package ca.unb.mobiledev.aceit;
+
+import java.io.Serializable;
+
+public class Hand implements Serializable {
+    private String card1 = "back";
+    private String card2 = "back";
+    private String card3 = "back";
+    private String card4 = "back";
+
+    public Hand(){}
+
+    public void addCard(String card, int pos){
+        if(pos == 0){
+            card1 = card;
+        }
+
+        if(pos == 1){
+            card1 = card;
+        }
+
+        if(pos == 2){
+            card1 = card;
+        }
+
+        if(pos == 3){
+            card1 = card;
+        }
+    }
+
+    public String getCard1(){
+        return this.card1;
+    }
+
+    public String getCard2(){
+        return this.card2;
+    }
+
+    public String getCard3(){
+        return this.card3;
+    }
+
+    public String getCard4(){
+        return this.card4;
+    }
+
+
+}
